e4dace7ed0d35bf0f3971dc831c7ae4592e520b1
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2023-10-30 00:22:49 +0300

    feat: add types for data and variables

---
+++ src/types/types.ts
@@ -0,0 +1,20 @@
+export type DataPlanet = {
+  climate: string;
+  created: string;
+  diameter: string;
+  edited: string;
+  films: Array<string>;
+  gravity: string;
+  name: string;
+  orbital_period: string;
+  population: string;
+  residents: Array<string>;
+  rotation_period: string;
+  surface_water: string;
+  terrain: string;
+  url: string;
+};
+
+export type DataSearch = {
+  searchInput: string | null;
+};
