3e7c820c6f0ccca0d2a54a2eb24cb1b4a507cf17
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2023-11-05 16:11:59 +0300

    refactor: change card with data to func component

--- src/components/dataCard/dataCard.tsx
+++ src/components/dataCard/dataCard.tsx
@@ -1,27 +1,20 @@
-import { Component } from "react";
-
 import styles from "./dataCard.module.css";
 import { DataPlanet } from "../../types/types";
 
-class DataCard extends Component<{ card: DataPlanet; key: number }> {
-  constructor(props: { card: DataPlanet; key: number }) {
-    super(props);
-  }
+function DataCard(props: { card: DataPlanet; key: number }): JSX.Element {
+  const { name, population, climate, terrain, gravity } = props.card;
 
-  render() {
-    const { name, population, climate, terrain, gravity } = this.props.card;
-    return (
-      <div className={styles.card}>
-        <div className={styles.name}>{name}</div>
-        <div className={styles.gravity}>Gravity: {gravity}</div>
-        <div className={styles.population}>Population: {population}</div>
-        <div className={styles.description}>
-          Description: Planet has the {climate} climate and represents {terrain}{" "}
-          as a surface
-        </div>
+  return (
+    <div className={styles.card}>
+      <div className={styles.name}>{name}</div>
+      <div className={styles.gravity}>Gravity: {gravity}</div>
+      <div className={styles.population}>Population: {population}</div>
+      <div className={styles.description}>
+        Description: Planet has the {climate} climate and represents {terrain}{" "}
+        as a surface
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default DataCard;
