96d05f95daddcdd15d3458a9bb76863da9d3ee2c
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2023-11-13 00:12:53 +0300

    feat: add tests for card component

---
+++ src/components/dataCard/DataCard.test.tsx
@@ -0,0 +1,77 @@
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import { BrowserRouter as Router } from "react-router-dom";
+import "@testing-library/jest-dom";
+
+import App from "../../App";
+import DataCard from "./DataCard";
+import {
+  mockPlanetBespin,
+  mockItemNumber,
+  mockPage,
+} from "../../mocks/mockPlanetBespin";
+
+describe("DataCard tests", () => {
+  test("Card component renders the relevant data", async () => {
+    render(
+      <Router>
+        <DataCard
+          key={mockPlanetBespin.name}
+          itemProp={mockPlanetBespin}
+          itemNumProp={mockItemNumber}
+          pageProp={mockPage}
+        />
+      </Router>,
+    );
+
+    const planetName = await screen.findByText(/bespin/i);
+    const gravityInfo = await screen.findByText(
+      "Gravity: 1.5 (surface), 1 standard (Cloud City)",
+    );
+    const populationInfo = await screen.findByText("Population: 6000000");
+    const description = await screen.findByText(
+      "Description: Planet has the temperate climate and represents gas giant as a surface",
+    );
+
+    expect(planetName).toBeInTheDocument();
+    expect(gravityInfo).toBeInTheDocument();
+    expect(populationInfo).toBeInTheDocument();
+    expect(description).toBeInTheDocument();
+  });
+
+  test("Clicking on a card calls the API function", async () => {
+    jest.spyOn(global, "fetch");
+
+    render(
+      <Router>
+        <App />
+      </Router>,
+    );
+
+    expect(global.fetch).toHaveBeenCalledWith(
+      "https://swapi.dev/api/planets/?page=1",
+    );
+
+    const cardBespin = await screen.findByText(/bespin/i);
+    await user.click(cardBespin);
+
+    expect(global.fetch).toHaveBeenCalledWith(
+      "https://swapi.dev/api/planets/6",
+    );
+    expect(global.fetch).toHaveBeenCalledTimes(2);
+  });
+
+  test("Clicking on a card opens a detailed card component", async () => {
+    render(
+      <Router>
+        <App />
+      </Router>,
+    );
+
+    const cardCurtain = await screen.findByRole("detailedCardCurtain");
+    const cardPanel = await screen.findByRole("detailedCardPanel");
+
+    expect(cardCurtain).toBeInTheDocument();
+    expect(cardPanel).toBeInTheDocument();
+  });
+});
