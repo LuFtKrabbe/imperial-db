e5fddc6b29e84238bb73d3732aa227467299b770
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2023-12-01 16:49:01 +0300

    feat: add react-router for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Main from "./pages/main/Main";
 
 function App(): JSX.Element {
   return (
-    <>
-      <Main />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="*" element={<h1>Not found</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
