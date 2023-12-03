6e937fd3a86b9d117452d4ead1dfdfa3b0c85693
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2023-12-03 03:36:39 +0300

    feat: add validation for uncontrolled form

--- src/pages/forms/UnCtrlForm.tsx
+++ src/pages/forms/UnCtrlForm.tsx
@@ -1,64 +1,180 @@
+import { useRef, useState } from "react";
 import styles from "./Forms.module.scss";
+import * as yup from "yup";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { definePasswordStrength } from "./definePasswordStrength";
+
+interface Errors {
+  [key: string]: string;
+}
 
 function UnCtrlForm(): JSX.Element {
+  const formRef = useRef<HTMLFormElement | null>(null);
+  const inputRefAge = useRef<HTMLInputElement | null>(null);
+  const inputRefMale = useRef<HTMLInputElement | null>(null);
+  const navigate = useNavigate();
+
+  const [errors, setErrors] = useState<Errors>({});
+  const [strength, setStrength] = useState({
+    color: "darkred",
+    fullness: "5%",
+  });
+
+  const validationSchema = yup.object().shape({
+    name: yup
+      .string()
+      .max(32)
+      .required()
+      .matches(/^[A-Z]/, "name must start with an uppercase letter"),
+    age: yup
+      .number()
+      .typeError("enter your age as a number")
+      .positive("age must be positive")
+      .integer()
+      .max(100),
+    gender: yup.string().required("choose your gender"),
+    country: yup.string().required("enter your country"),
+    email: yup.string().email().required("enter your e-mail"),
+    password: yup
+      .string()
+      .required("enter your password")
+      .test("passwordStrength", `weak password`, function (value) {
+        const passwordStrength = definePasswordStrength(value);
+        setStrength(passwordStrength);
+        return passwordStrength.fullness === "100%" ? true : false;
+      })
+      .matches(/[0-9]/, "at least 1 number")
+      .matches(/[a-z]/, "at least 1 lowercase character")
+      .matches(/[A-Z]/, "at least 1 uppercase character")
+      .matches(/[@$!%*?&()_+\-=\[\]{};]/, "at least 1 special character"),
+    confirmPassword: yup
+      .string()
+      .required("enter your password")
+      .oneOf([yup.ref("password")], "passwords must match"),
+    image: yup.string().required("image is required"),
+    termsConditions: yup
+      .string()
+      .required("Terms & Сonditions must be accepted"),
+  });
+
+  const onFormSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    setStrength({ color: "darkred", fullness: "5%" });
+    if (formRef.current) {
+      const formData = new FormData(formRef.current);
+      validationSchema
+        .validate(Object.fromEntries(formData), {
+          abortEarly: false,
+        })
+        .then((valid) => {
+          if (valid) {
+            console.log(Object.fromEntries(formData));
+            navigate("/main");
+          } else {
+            console.log("Form validation failed");
+          }
+        })
+        .catch((error) => {
+          if (error instanceof yup.ValidationError) {
+            setErrors(
+              error.inner.reduce((allErrors, currentError) => {
+                allErrors[currentError.path as string] = currentError.message;
+                return allErrors;
+              }, {} as Errors),
+            );
+          }
+          console.log("Validation error: ", error);
+          console.log("Validation errors: ", errors);
+        });
+    }
+  };
+
   return (
     <>
       <div className={styles.title}>UNCONTROLLED FORM</div>
       <Link className={styles.backButton} to={"/main"}>
         ← BACK
       </Link>
-      <form>
+      <form ref={formRef} onSubmit={onFormSubmit} noValidate>
         <div className={styles.fieldsContainer}>
           <div className={styles.field}>
             <label htmlFor="name">Name</label>
-            <input type="text" id="username" name="username"></input>
+            <input type="text" id="name" name="name"></input>
           </div>
+          {errors.name && <p className={styles.errorMessage}>{errors.name}</p>}
           <div className={styles.field}>
             <label htmlFor="age">Age</label>
-            <input type="text" id="age" name="age"></input>
+            <input type="text" id="age" name="age" ref={inputRefAge}></input>
           </div>
+          {errors.age && <p className={styles.errorMessage}>{errors.age}</p>}
           <div className={styles.field}>
             <label>Gender</label>
             <div className={styles.genderContainer}>
               <input
                 type="radio"
                 id="male"
+                value="male"
                 name="gender"
                 className={styles.radio}
+                ref={inputRefMale}
               ></input>
               <label htmlFor="male">Male</label>
               <input
                 type="radio"
                 id="female"
+                value="female"
                 name="gender"
                 className={styles.radio}
               ></input>
               <label htmlFor="female">Female</label>
             </div>
           </div>
+          {errors.gender && (
+            <p className={styles.errorMessage}>{errors.gender}</p>
+          )}
           <div className={styles.field}>
             <label htmlFor="country">Country</label>
             <input type="input" id="country" name="country"></input>
           </div>
+          {errors.country && (
+            <p className={styles.errorMessage}>{errors.country}</p>
+          )}
           <div className={styles.divider}></div>
           <div className={styles.field}>
             <label htmlFor="email">E-mail</label>
             <input type="email" id="email" name="email"></input>
           </div>
+          {errors.email && (
+            <p className={styles.errorMessage}>{errors.email}</p>
+          )}
           <div className={styles.field}>
             <label htmlFor="password">Password</label>
             <input type="text" id="password" name="password"></input>
           </div>
+          <div className={styles.strengthBar}>
+            <div
+              className={styles.strength}
+              style={{
+                backgroundColor: strength.color,
+                width: strength.fullness,
+              }}
+            ></div>
+          </div>
+          {errors.password && (
+            <p className={styles.errorMessage}>{errors.password}</p>
+          )}
           <div className={styles.field}>
-            <label htmlFor="confirm-password">Confirm password</label>
+            <label htmlFor="confirmPassword">Confirm password</label>
             <input
               type="text"
-              id="confirm-password"
-              name="confirm-password"
+              id="confirmPassword"
+              name="confirmPassword"
             ></input>
           </div>
+          {errors.confirmPassword && (
+            <p className={styles.errorMessage}>{errors.confirmPassword}</p>
+          )}
           <div className={styles.divider}></div>
           <div className={styles.field}>
             <label htmlFor="image">Image</label>
@@ -69,15 +185,22 @@ function UnCtrlForm(): JSX.Element {
               accept="image/png, image/jpeg"
             ></input>
           </div>
+          {errors.image && (
+            <p className={styles.errorMessage}>{errors.image}</p>
+          )}
           <div className={styles.field}>
-            <label htmlFor="terms-conditions">Terms & Сonditions</label>
+            <label htmlFor="termsConditions">Terms & Сonditions</label>
             <input
               type="checkbox"
-              id="terms-conditions"
-              name="terms-conditions"
+              id="termsConditions"
+              value="accepted"
+              name="termsConditions"
               className={styles.terms}
             ></input>
           </div>
+          {errors.termsConditions && (
+            <p className={styles.errorMessage}>{errors.termsConditions}</p>
+          )}
         </div>
         <button type="submit" className={styles.submitButton}>
           SUBMIT
---
+++ src/pages/forms/definePasswordStrength.tsx
@@ -0,0 +1,30 @@
+export function definePasswordStrength(value: string) {
+  let strength = { color: "darkred", fullness: "5%" };
+
+  if (value) {
+    let validConditions = 0;
+    const lower = /[a-z]/;
+    const upper = /[A-Z]/;
+    const number = /[0-9]/;
+    const special = /[@$!%*?&()_+\-=\[\]{};]/;
+    const conditions = [lower, upper, number, special];
+    conditions.forEach((condition) =>
+      condition.test(value) ? validConditions++ : null,
+    );
+    switch (validConditions) {
+      case 1:
+        strength = { color: "darkred", fullness: "25%" };
+        break;
+      case 2:
+        strength = { color: "darkorange", fullness: "50%" };
+        break;
+      case 3:
+        strength = { color: "darkgoldenrod", fullness: "75%" };
+        break;
+      case 4:
+        strength = { color: "darkgreen", fullness: "100%" };
+    }
+  }
+
+  return strength;
+}
